598bb801de6edfa18b3ef5dcc455ba7d1909c32c
NadirCianid <101037555+NadirCianid@users.noreply.github.com>
2022-03-17 21:34:46 +0300

    Portfolio
    -Добавил портфолио
    -Добавил возможность переключаться между портфолио и биржей
    -Сделал рабочей кнопку выхода
    -Добавил переход при выборе уровня сложности на биржу

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] GameObject marketScreen;
+    [SerializeField] GameObject portfolioScreen;
+    public void Exit()
+    {
+        Application.Quit();
+    } 
+
+    public void Portfolio()
+    {
+        marketScreen.SetActive(false);
+        portfolioScreen.SetActive(true);
+    }
+
+    public void Market()
+    {
+        marketScreen.SetActive(true);
+        portfolioScreen.SetActive(false);
+    }
+
+    public void MainScreen()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
