4346af196fa9dc29eac4a69feee8e9b3cf3b2b22
NadirCianid <101037555+NadirCianid@users.noreply.github.com>
2022-03-19 01:04:55 +0300

    ColoredPercentOfChanges&ConfirmationTable
    -Добавил отображение последнего изменения цены акции в виде цветных процентов;
    -Добавил табличку подтверждения при покупке/продаже товара;

--- Assets/Scripts/Buying/Purchasing.cs
+++ Assets/Scripts/Buying/Purchasing.cs
@@ -9,9 +9,34 @@ public class Purchasing : MonoBehaviour
 {
     [SerializeField] TMP_InputField amountText;
     [SerializeField] int amount;
-    public void EnterAmount()
+    [SerializeField] GameObject confirmationTable;
+    [SerializeField] GameObject prePurchaseTable;
+    [SerializeField] TextMeshProUGUI question;
+    int sign;
+    public void Buy()
     {
-        amount = Convert.ToInt32(amountText.text);        
-        
+        confirmationTable.SetActive(true);
+        prePurchaseTable.SetActive(false);
+        question.text = "Are you want to buy " + Convert.ToInt32(amountText.text) + " ?" ;
+        sign=1;
+    }
+     public void Sell()
+    {
+        confirmationTable.SetActive(true);
+        prePurchaseTable.SetActive(false);
+        question.text = "Are you want to sell " + Convert.ToInt32(amountText.text) + " ?" ;
+        sign=-1;
+    }
+
+    public void ConfirmAmount()
+    {
+        amount = sign*Convert.ToInt32(amountText.text);
+        Cancel(); 
+    }
+    public void Cancel()
+    {
+        amountText.text = "Enter amount";
+        confirmationTable.SetActive(false);
+        prePurchaseTable.SetActive(true); 
     }
 }
--- Assets/Scripts/CompaniesInfo/ShortInfo.cs
+++ Assets/Scripts/CompaniesInfo/ShortInfo.cs
@@ -5,16 +5,22 @@ using UnityEngine.UI;
 using TMPro;
 using UnityEditor;
 using UnityEngine.Events;
+using System;
 
 public class ShortInfo : MonoBehaviour
 {
     [SerializeField] Button button;
     [SerializeField] int index;
-    [SerializeField] TextMeshProUGUI companyName;  
+    [SerializeField] TextMeshProUGUI companyName;
+    [SerializeField] TextMeshProUGUI percentOfChange;  
     [SerializeField] TextMeshProUGUI price;
     CompanySO company;
     DetailedInfoManager companies;
     DetailedInfoManager setCompanies;
+    Color red = new Color(0.8f, 0.09f, 0.09f, 1);
+    Color green = new Color(0.09f, 0.7f, 0.1f, 1f);
+    float previousPrice;
+    double deltaPrice;
 
     void Awake() 
     {
@@ -28,10 +34,29 @@ public class ShortInfo : MonoBehaviour
         button.onClick.AddListener(()=>{DetailedInfoManager._instance.UpdateAllInformation(index);});
         companyName.text = company.returnNameOfCompany();
         price.text = company.GetPrice().ToString();
+        previousPrice = company.GetPrice();
     }
     private void Update()
     {
+        
+        if(previousPrice > company.GetPrice())
+        {
+            percentOfChange.color = red;
+            
+            deltaPrice = Math.Round((previousPrice/company.GetPrice()-1)*100,2);
+            percentOfChange.text = deltaPrice.ToString()+"%";
+        }
+        else if(previousPrice < company.GetPrice())
+        {
+            percentOfChange.color = green;
+            
+            deltaPrice = Math.Round((company.GetPrice()/previousPrice-1)*100, 2);
+            percentOfChange.text = deltaPrice.ToString()+"%";
+        }
         price.text = company.GetPrice().ToString();
+
+        previousPrice = company.GetPrice();
+       
     }
 
     int GetIndex() => index;
