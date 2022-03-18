aa8ec97cba41385d359ee34ea6273f9ab7760690
NadirCianid <101037555+NadirCianid@users.noreply.github.com>
2022-03-18 13:15:26 +0300

    Purchasing3
    -Работает ввод текста

--- Assets/Scripts/Buying/Purchasing.cs
+++ Assets/Scripts/Buying/Purchasing.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
+using System;
 
 public class Purchasing : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI amountText;
-    float amount;
-   public void EnterAmount()
-   {
-       amount = Convert.ToFloat(amountText);
-   }
+    [SerializeField] TMP_InputField amountText;
+    [SerializeField] int amount;
+    public void EnterAmount()
+    {
+        amount = Convert.ToInt32(amountText.text);        
+        
+    }
 }
--- Assets/Scripts/Portfolio.cs
+++ Assets/Scripts/Portfolio.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-using 
+ 
 
 
 public class Portfolio : MonoBehaviour
