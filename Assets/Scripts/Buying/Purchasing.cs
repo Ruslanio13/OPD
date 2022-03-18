7c3e8547def4bc12b9e7697774b4cf31d3dccd36
NadirCianid <101037555+NadirCianid@users.noreply.github.com>
2022-03-19 01:06:51 +0300

    ColoredPercentOfChanges&ConfirmationTable
    -Добавил цветное отображения процента последнего изменения цены;
    -Добавил табличку подтверждения покупки/продажи;

--- Assets/Scripts/Buying/Purchasing.cs
+++ Assets/Scripts/Buying/Purchasing.cs
@@ -9,34 +9,9 @@ public class Purchasing : MonoBehaviour
 {
     [SerializeField] TMP_InputField amountText;
     [SerializeField] int amount;
-    [SerializeField] GameObject confirmationTable;
-    [SerializeField] GameObject prePurchaseTable;
-    [SerializeField] TextMeshProUGUI question;
-    int sign;
-    public void Buy()
+    public void EnterAmount()
     {
-        confirmationTable.SetActive(true);
-        prePurchaseTable.SetActive(false);
-        question.text = "Are you want to buy " + Convert.ToInt32(amountText.text) + " ?" ;
-        sign=1;
-    }
-     public void Sell()
-    {
-        confirmationTable.SetActive(true);
-        prePurchaseTable.SetActive(false);
-        question.text = "Are you want to sell " + Convert.ToInt32(amountText.text) + " ?" ;
-        sign=-1;
-    }
-
-    public void ConfirmAmount()
-    {
-        amount = sign*Convert.ToInt32(amountText.text);
-        Cancel(); 
-    }
-    public void Cancel()
-    {
-        amountText.text = "Enter amount";
-        confirmationTable.SetActive(false);
-        prePurchaseTable.SetActive(true); 
+        amount = Convert.ToInt32(amountText.text);        
+        
     }
 }
