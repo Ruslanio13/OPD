af4ba0380b20a73d4badd52a874d24f48d5f66af
NadirCianid <101037555+NadirCianid@users.noreply.github.com>
2022-03-18 12:37:00 +0300

    Inputfield
    -Добавил поле ввода для покупки/продажи

---
+++ Assets/Scripts/Buying/Purchasing.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Purchasing : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI amountText;
+    float amount;
+   public void EnterAmount()
+   {
+      // amount = amountText;
+   }
+}
