eea9d4cbe7480233ee86cee13829e22611453ee4
Ruslanio13 <rusyaaaaa13@gmail.com>
2022-04-12 16:48:23 +0300

    обнова

--- Assets/Scripts/CompaniesInfo/DetailedInfoManager.cs
+++ Assets/Scripts/CompaniesInfo/DetailedInfoManager.cs
@@ -13,6 +13,7 @@ public class DetailedInfoManager : MonoBehaviour
     [SerializeField] public Graph _graph;
     [SerializeField] DetailedInfo table;
     [SerializeField] private GameObject _shortInfoPrefab;
+    [SerializeField] private GameObject _notification;
     [SerializeField] private Button _selRubButton;
     [SerializeField] private Button _selEurButton;
     [SerializeField] private Button _selDolButton;
@@ -139,7 +140,7 @@ public class DetailedInfoManager : MonoBehaviour
 
 
         }
-        
+
         if (Input.GetKeyDown(KeyCode.LeftArrow))
             _graph.EnlargeScale();
 
